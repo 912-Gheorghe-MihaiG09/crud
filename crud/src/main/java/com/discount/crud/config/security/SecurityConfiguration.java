93779cbc5277a075b27454233eadf7fa9b5581dd
mihaigheorghe <mihai.gheorghe@partners.instagrid.co>
2024-05-20 12:20:03 +0300

    undo cors

--- crud/src/main/java/com/discount/crud/config/security/SecurityConfiguration.java
+++ crud/src/main/java/com/discount/crud/config/security/SecurityConfiguration.java
@@ -43,8 +43,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http    .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
+        http    .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL).permitAll()
                                 .anyRequest()
